571a1a04bb5bd3648299fdd9750ac9da8b33e43e
gabrigomezayuso <ggomezayuso@gmail.com>
2021-02-03 19:24:17 +0100

    admin

---
+++ src/app/PHP/Login.php
@@ -0,0 +1,33 @@
+<?php
+  header('Access-Control-Allow-Origin: *');
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+  $json = file_get_contents('php://input'); // RECIBE EL JSON DE ANGULAR
+
+  $params = json_decode($json); // DECODIFICA EL JSON Y LO GUARADA EN LA VARIABLE
+
+  require("db.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
+
+  $conexion = conexion(); // CREA LA CONEXION
+
+  // REALIZA LA QUERY A LA DB
+  $resultado = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$params->usuario' AND contrasena='$params->contrasena'");
+
+    class Result {}
+
+    // GENERA LOS DATOS DE RESPUESTA
+    $response = new Result();
+
+    if($resultado->num_rows > 0) {
+        $response->resultado = 'OK';
+        $response->mensaje = 'LOGIN EXITOSO';
+    } else {
+        $response->resultado = 'FAIL';
+        $response->mensaje = 'LOGIN FALLIDO';
+    }
+
+    header('Content-Type: application/json');
+
+    echo json_encode($response); // MUESTRA EL JSON GENERADO
+
+?>
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
  { path: 'login', component: LoginComponent },
  { path: 'register', component: RegisterComponent },
  { path: 'register-admin', component: RegisterAdminComponent },
+ { path: 'home', component: HomeComponent },
  { path: '**', redirectTo: 'login' }
 
 ];
