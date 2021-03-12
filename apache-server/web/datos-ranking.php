2071f1db854000e12aa9f7cf098d354f66efc20c
DAW2 <ggomezayuso@gmail.com>
2021-03-12 18:32:23 +0100

    RANKING POR CODIGO

--- apache-server/web/datos-ranking.php
+++ apache-server/web/datos-ranking.php
@@ -17,12 +17,12 @@
 
 
 
-$query = $mysqli -> query ("select a.nombre ,a.usuario,a.apellido, u2.puntuacion, e2.nombreEquipo
+$query = $mysqli -> query ("select r.nombre_ranking, a.nombre ,a.usuario,a.apellido, u2.puntuacion, e2.nombreEquipo
 from rankings r
 inner join usuariosranking u2 on u2.idRanking = r.id_ranking
 inner join equiposranking e2  on e2.idRanking = r.id_ranking
 inner join alumnos a on a.idUsuario = u2.idUsuario
-where r.nombre_ranking ='$params->nombre_ranking'
+where r.id_ranking ='$params->nombre_ranking'
 order by u2.puntuacion desc ");
 
 
