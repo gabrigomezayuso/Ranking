762f7779c438afb5b783d5a41573c6f8a46e25b4
sergibertran <bigres24@gmail.com>
2021-04-06 18:53:32 +0200

    fff

--- apache-server/web/createEntregas.php
+++ apache-server/web/createEntregas.php
@@ -1,21 +1,21 @@
 <?php
   header('Access-Control-Allow-Origin: *');
   header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-
+  $datos;
   $json = file_get_contents('php://input'); // RECIBE EL JSON DE ANGULAR
 
   $params = json_decode($json); // DECODIFICA EL JSON Y LO GUARADA EN LA VARIABLE
 
   require("db.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
-
+  $id=$params->idRanking;
+  $nombre=$params->nombreEntrega;
   $conexion = conexion(); // CREA LA CONEXION
 
-  $mysqli = new mysqli('192.168.3.26', 'DAW2_GamifikG6', 'aGamifikG61', 'daw2_gamifikg6');
+   $resultadoNoRepetir = mysqli_query($conexion, " INSERT INTO daw2_gamifikg6.entrega ( idranking, nentrega) VALUES( $id,$nombre');");
 
-   $resultadoNoRepetir = mysqli_query($conexion, "INSERT INTO entrega (idranking, nentrega)  values ($params->idRanking,$params->nombreEntrega)");
 
     header('Content-Type: application/json');
 
-    echo json_encode($params->idRanking); // MUESTRA EL JSON GENERADO
+    echo json_encode($id+$nombre); // MUESTRA EL JSON GENERADO
 
 ?>
