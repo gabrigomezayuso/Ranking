1da1958146af4e86545210958201b5a7c76e513d
alexrp202 <72034440+alexrp202@users.noreply.github.com>
2021-04-09 16:29:45 +0200

    sentencia datos ranking

--- apache-server/web/datos-ranking.php
+++ apache-server/web/datos-ranking.php
@@ -18,17 +18,22 @@ $x = 0;
 
 
 
-$query = $mysqli->query("select  DISTINCT r.nombre_ranking, a.nombre ,a.idUsuario,a.usuario,a.apellido, p2.puntuacion ,  e2.nombreEquipo, r.id_ranking
-from rankings r
-inner join usuariosranking u2 on u2.idRanking = r.id_ranking
-inner join equiposranking e2  on e2.idUsuario = u2.idUsuario
-inner join entrega e3  on e3.idranking = r.id_ranking
-left join puntuacionentrega p2  on p2.identrega = e3.identrega and p2.idusuario = u2.idUsuario
-inner join alumnos a on a.idUsuario = u2.idUsuario
-where e2.idRanking = r.id_ranking and e2.idRanking = 429583 and e3.nentrega = 'coldwar'
-order by u2.puntuacion desc");
+$query = $mysqli->query("select r2.nombre_ranking,a2.nombre,a2.idUsuario, a2.usuario, a2.apellido, e2.nombreEquipo, r2.id_ranking, sum(puntuacion)
+from puntuacionentrega p
+inner join rankings r2 on r2.id_ranking = p.idranking
+inner join alumnos a2 on a2.idUsuario = p.idusuario
+inner join equiposranking e2 on e2.idUsuario = p.idusuario
+group by a2.idusuario ");
 
-
+// select  DISTINCT r.nombre_ranking, a.nombre ,a.idUsuario,a.usuario,a.apellido, p2.puntuacion ,  e2.nombreEquipo, r.id_ranking
+// from rankings r
+// inner join usuariosranking u2 on u2.idRanking = r.id_ranking
+// inner join equiposranking e2  on e2.idUsuario = u2.idUsuario
+// inner join entrega e3  on e3.idranking = r.id_ranking
+// left join puntuacionentrega p2  on p2.identrega = e3.identrega and p2.idusuario = u2.idUsuario
+// inner join alumnos a on a.idUsuario = u2.idUsuario
+// where e2.idRanking = r.id_ranking and e2.idRanking = 429583 and e3.nentrega = 'coldwar'
+// order by u2.puntuacion desc
 
 while ($valores = mysqli_fetch_array($query)) {
   $array[$x] = $valores;
